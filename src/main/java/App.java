c5d210ff4dab977683079562983ff9be81e58c84
lanarokip <lankipkoecharo@gmail.com>
2021-09-13 13:15:45 +0300

    added a form where one can input the heros

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,18 @@
+import static spark.Spark.*;
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class App {
+    public static void main(String[] args) {
+        staticFileLocation("/public");
+        get("/",(request, response) -> {
+            Map<String,Object> model = new HashMap<>();
+            return  new ModelAndView(new HashMap(),"index.hbs");
+        },new HandlebarsTemplateEngine());
+    }
 }
